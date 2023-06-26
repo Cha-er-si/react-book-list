07e3d0c4d7fc933dca6c7f47f7423cd3c4898842
Cha-er-si <vpcharles31@gmail.com>
2023-06-26 16:07:21 +0800

    Added BookList functionality and Remove Book functionality'

---
+++ src/components/BookDetails.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import { BookContext } from "../contexts/BookContext";
+
+const BookDetails = ({ book }) => {
+  const { removeBook } = useContext(BookContext);
+  return (
+    <li
+      onClick={() => {
+        removeBook(book.id);
+      }}
+    >
+      <div className="title">Title: {book.title}</div>
+      <div className="author">Author: {book.author}</div>
+    </li>
+  );
+};
+
+export default BookDetails;
